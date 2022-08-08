6336c899f1594cad4f7b9384272368fab936ee3d
Tomislav Pandzic <tome91@windowslive.com>
2022-08-08 23:30:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+TODO creation in React JS, with the ability to remove individual items, or batch of them. Items are saved in the Local Storage.
---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    message: "",
+    type: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // display alert
+
+      showAlert(true, "danger", "Please enter a value");
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "Item updated");
+    } else {
+      showAlert(true, "success", "Item added to the list");
+
+      const newItem = { id: new Date().getTime().toString(), title: name };
+
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  const showAlert = (show = false, type = "", message = "") => {
+    setAlert({ show: show, type, message });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "List cleared");
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, "danger", "Item removed from the list");
+    // if the item.id matches to id that's passed, then  dont return it from this filter
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="example. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+
+      {list && list.length > 0 ? (
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
+            Clear Items
+          </button>{" "}
+        </div>
+      ) : null}
+    </section>
+  );
+}
+
+export default App;
